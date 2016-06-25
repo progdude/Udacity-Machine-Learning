127495ce20c3b6336d9827ecd443a5f1c2532646
progdude <architrathi10@gmail.com>
2016-06-25 12:32:12 -0400

    added svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,8 +12,6 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -21,21 +19,33 @@ from sklearn.metrics import accuracy_score
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+
+t0 = time()
+
 #########################################################
 ### your code goes here ###
 
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
 
 
-clf = SVC(kernel='linear')
+from sklearn.svm import SVC
+clf = SVC(C=10000)
+clf.fit(features_train, labels_train)
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
-clf.fit(features_train, features_test)
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.0,
-  kernel='linear', max_iter=-1, probability=False, random_state=None,
-  shrinking=True, tol=0.001, verbose=False)
-clf.score(features_test,labels_test)
+pred = clf.predict(features_test)
+
+chris = 0
+
+for i in range(0, len(pred)):
+	if(pred[i]==1):
+		chris+=1
+
+print chris
+# print pred[10]
+# print pred[26]
+# print pred[50]
+
 #########################################################
-
 
